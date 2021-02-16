0bb52605b37abc8b617e71ac829d5e94aebab9e8
Paulo Cesar <pc@pc.eti.br>
2021-02-16 13:34:55 -0300

    Testes diversos com código Java

---
+++ ws-eclipse/teclas/src/teclas/Main.java
@@ -0,0 +1,25 @@
+package teclas;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		
+		// String x;
+		// x = sc.next();
+		
+		// int x;
+		// x = sc.nextInt();
+		
+		double x;
+		x = sc.nextDouble();
+		
+		System.out.println("Voce digitou: " + x);
+		
+		
+		sc.close();
+	}
+
+}
---
+++ ws-eclipse/teste_processamento2/src/teste_processamento2/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sca = new Scanner(System.in);
+				
+				String s1, s2, s3;
+				
+				s1 = sca.nextLine();
+				s2 = sca.nextLine();
+				s3 = sca.nextLine();
+				
+				System.out.println("Dados digitados: ");
+				System.out.println(s1);
+				System.out.println(s2);
+				System.out.println(s3);
+
+				
+				sca.close();
+				
+	}
+
+}
