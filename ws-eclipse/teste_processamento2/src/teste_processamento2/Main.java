332907c21fd46d25110d5b7a4ad40dcd625ecc89
Paulo Cesar <pc@pc.eti.br>
2021-02-17 00:31:32 -0300

    Funcoes matematicas

--- ws-eclipse/teste_processamento2/src/teste_processamento2/Main.java
+++ ws-eclipse/teste_processamento2/src/teste_processamento2/Main.java
@@ -1,3 +1,4 @@
+package teste_processamento2;
 
 import java.util.Scanner;
 
@@ -8,13 +9,17 @@ public class Main {
 		
 		Scanner sca = new Scanner(System.in);
 				
+				int x;
 				String s1, s2, s3;
 				
+				x = sca.nextInt();
+					 sca.nextLine();
 				s1 = sca.nextLine();
 				s2 = sca.nextLine();
 				s3 = sca.nextLine();
 				
 				System.out.println("Dados digitados: ");
+				System.out.println(x);
 				System.out.println(s1);
 				System.out.println(s2);
 				System.out.println(s3);
